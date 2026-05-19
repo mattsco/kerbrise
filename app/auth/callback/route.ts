ddf1e69a659c9117d69f245ea2020f6ec540d15f
Matthieu Scordia <Matthieu.sco@gmail.com>
2026-05-19 21:21:25 +0800

    Delete route.ts

--- app/auth/callback/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse, type NextRequest } from "next/server";
-
-export async function GET(request: NextRequest) {
-  // En implicit flow, le token est dans le hash (#access_token=...)
-  // qui n'est pas envoyé au serveur. On délègue à une page client.
-  const { origin } = new URL(request.url);
-  return NextResponse.redirect(`${origin}/auth/process`);
-}
