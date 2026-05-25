bb371124a252331f2061e9605f8eeb70305cc757
Matthieu Scordia <matthieu.sco@gmail.com>
2026-05-25 08:49:38 +0800

    remove loading skeletons (back to default smooth transition)

--- app/dashboard/calendrier/loading.tsx
+++
@@ -1,9 +0,0 @@
-import PageLoading from "@/components/PageLoading";
-
-export default function Loading() {
-  return (
-    <div className="p-4">
-      <PageLoading title="Calendrier" />
-    </div>
-  );
-}
