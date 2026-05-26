b2fe800f14dce78dff61196cd9ba5676c83ade8e
Matthieu Scordia <Matthieu.sco@gmail.com>
2026-05-26 15:41:05 +0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,10 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: { extend: {} },
-  plugins: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
 };
+
 export default config;
