afae6fe9784d8b259ccdaf228fedf3791c4d6079
Matthieu Scordia <Matthieu.sco@gmail.com>
2026-05-29 22:01:06 +0800

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -5,6 +5,10 @@
  * Pour l'instant, on les modifie ici, commit, push, et c'est en ligne en ~1min.
  */
 
+import packageJson from "@/package.json";
+
+export const APP_VERSION = packageJson.version;
+
 /**
  * Si true, n'importe quel membre de la famille peut réserver une période d'été.
  * Si false (défaut), seul le chef de famille (is_family_head) peut.
